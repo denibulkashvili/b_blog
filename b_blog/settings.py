22f95ba2f3a843198c1028969631edab82de021d
denibulkashvili <denibulkashvili@gmail.com>
2019-05-09 00:27:06 +0700

    pylint warnings

--- b_blog/settings.py
+++ b_blog/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-import environ
-
+# pylint: disable=invalid-name
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
--- b_blog/urls.py
+++ b_blog/urls.py
@@ -16,4 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# pylint: disable=invalid-name
 urlpatterns = [path("admin/", admin.site.urls)]
