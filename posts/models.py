46cce7639e41cd6a8ebc49234e7dc820a8f4bcf0
denibulkashvili <denibulkashvili@gmail.com>
2019-05-09 01:46:21 +0700

    Add initial models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    """Creates a Post model"""
+    title = models.CharField(max_length=200, verbose_name="post_title")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
+
+class Tag(models.Model):
+    """Creates a Tag model"""
+    name = models.CharField(max_length=20, verbose_name="tag_name")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("tag_detail", kwargs={"pk": self.pk})
