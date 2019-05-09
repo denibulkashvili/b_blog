c09e683904b380c1be96eb27c848eefd9dc34134
denibulkashvili <denibulkashvili@gmail.com>
2019-05-09 10:01:05 +0700

    Basic post views, templates and urlcon

--- b_blog/urls.py
+++ b_blog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # pylint: disable=invalid-name
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.HomePageView.as_view(), name="home"),
     path("about/", views.AboutPageView.as_view(), name="about"),
+    path("posts/" ,include("posts.urls", namespace="posts")),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
+"""Views module for posts app"""
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Tag
 
 # Create your views here.
+class PostListView(ListView):
+    """Creates post list view"""
+    model = Post
+    template_name = "post_list.html"
