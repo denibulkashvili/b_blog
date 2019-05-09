1224279d955ededa5cc0f7290b365b4951f21972
denibulkashvili <denibulkashvili@gmail.com>
2019-05-09 11:10:29 +0700

    Update post tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,14 +2,11 @@ from django.test import TestCase
 from django.urls import reverse
 
 from posts import views
-# from posts.models import Post, Tag
+from posts.models import Post, Tag
 
 
 class HomePageTests(TestCase):
     """Tests home page"""
-
-    # def setUp(self):
-    #     self.post = Post.objects.create(title="Test")
 
     def test_home_page_status_code(self):
         response = self.client.get("/")
@@ -30,15 +27,41 @@ class HomePageTests(TestCase):
 
 class PostListPageTests(TestCase):
     """Test post list view"""
+    def setUp(self):
+        self.post = Post.objects.create(title="Test")
+
     def test_post_list_template(self):
         response = self.client.get(reverse("posts:post_list"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "posts/post_list.html")
 
+    def test_post_list_page_renders_posts(self):
+        pass
+
 
 class PostDetailPageTests(TestCase):
-    pass
+    """Test post detail view"""
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(
+            title="Hello World!",
+            description="Descriptions: Test post",
+            content="Content: This is a test post."
+        )
 
+    def setUp(self):
+        self.post = Post.objects.get(id=1)        
+
+    def test_post_detail_template(self):
+        response = self.client.get("/posts/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "posts/post_detail.html")
+
+    def test_post_detail_page_displays_post_details(self):
+        response = self.client.get("/posts/1/")
+        self.assertContains(response, "Hello World")
+        self.assertContains(response, "Descriptions: Test post")
+        self.assertContains(response, "Content: This is a test post.")
 
 class TagListPageTests(TestCase):
     pass
